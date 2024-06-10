dc88b7143b9fad48d4a96812d069b49db9b6211c
JeciraMiguel <166519131+JeciraMiguel@users.noreply.github.com>
2024-06-10 12:48:29 +0200

    Third Screen and logic

--- app/src/main/java/com/example/weatherapp/ThirdScreen.kt
+++ app/src/main/java/com/example/weatherapp/ThirdScreen.kt
@@ -1,20 +1,35 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ThirdScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_third_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val listView: ListView = findViewById(R.id.list_view)
+        val btnBack: Button = findViewById(R.id.btn_back)
+
+        val dates = intent.getStringArrayListExtra("dates")
+        val minTemps = intent.getIntegerArrayListExtra("minTemps")
+        val maxTemps = intent.getIntegerArrayListExtra("maxTemps")
+        val weatherConditions = intent.getStringArrayListExtra("weatherConditions")
+
+        val detailsList = ArrayList<String>()
+        for (i in dates!!.indices) {
+            val detail = "Date: ${dates[i]}, Min Temp: ${minTemps!![i]}°C, Max Temp: ${maxTemps!![i]}°C, Condition: ${weatherConditions!![i]}"
+            detailsList.add(detail)
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, detailsList)
+        listView.adapter = adapter
+
+        btnBack.setOnClickListener {
+            finish()
         }
     }
 }
